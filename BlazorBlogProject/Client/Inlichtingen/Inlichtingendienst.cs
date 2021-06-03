29c1e12cb8e3d9c3850937574275439a18331dab
MRAsoft <info.mrasoft@gmail.com>
2021-06-03 20:24:22 +0200

    Add project files.

---
+++ BlazorBlogProject/Client/Inlichtingen/Inlichtingendienst.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBlogProject.Client.Inlichtingen
+{
+    public class Inlichtingendienst
+    {
+        public string InlichtingenLand { get; private set; }
+
+        public string InlichtingenProduct { get; private set; }
+
+        public bool DesInformatie { get; private set; }
+
+        // A1. Vergaren door te spelen inlichtingen
+        public void VergaarInlichtingen(ComponentBase Bron, string InlichtingenLand, string InlichtingenProduct)
+        {
+            // Verzamel de gegevens
+            this.InlichtingenLand = InlichtingenLand;
+            this.InlichtingenProduct = InlichtingenProduct;
+
+            // Aangeven dat de inlichtingen zijn vergaard en verstuurd kunnen worden naar de Inlichtingendienst
+            InlichtingenSamengesteld(Bron, InlichtingenLand, InlichtingenProduct);
+        }
+
+        // A2. Signaal naar de Inlichtingendienst zodat die weet dat de inlichtingenofficier gegevens heeft gestuurd
+        private void InlichtingenSamengesteld(ComponentBase Bron, string InlichtingenLand, string InlichtingenProduct)
+            => InlichtingenVerstuurd?.Invoke(Bron, InlichtingenLand, InlichtingenProduct);
+
+        // A3. Signaal dat gedetecteerd wordt door de Inlichtingendienst, inlichtingen van de inlichtingenofficier
+        public event Action<ComponentBase, string, string> InlichtingenVerstuurd;
+
+        // B1. Samenstellen Desinformatie
+        public void MaakDesInformatie(ComponentBase Bron, string InlichtingenLand, string InlichtingenProduct, bool DesInformatie)
+        {
+            // De te versturen desinformatie 
+            this.InlichtingenLand = InlichtingenLand;
+            this.InlichtingenProduct = InlichtingenProduct;
+            this.DesInformatie = DesInformatie;
+
+            // Aangeven dat de desinformate is samengesteld en verstuurd kan worden naar de inlichtingenofficier
+            DesInformatieSamengesteld(Bron, InlichtingenLand, InlichtingenProduct, DesInformatie);
+        }
+
+        // B2. Signaal naar de Inlichtingenofficier zodat die weet dat de Inlichtingendienst desinformatie heeft gestuurd
+        private void DesInformatieSamengesteld(ComponentBase Bron, string InlichtingenLand, string InlichtingenProduct, bool DesInformatie)
+            => DesInformatieVerstuurd?.Invoke(Bron, InlichtingenLand, InlichtingenProduct, DesInformatie);
+
+        // B3. Signaal dat gedetecteerd wordt door de Inlichtingenofficier, desinformatie van de Inlichtingendienst
+        public event Action<ComponentBase, string, string, bool> DesInformatieVerstuurd;
+
+    }
+}
---
+++ BlazorBlogProject/Client/Program.cs
@@ -0,0 +1,35 @@
+using BlazorBlogProject.Client.Inlichtingen;
+using BlazorBlogProject.Client.Model;
+using BlazorBlogProject.Client.ViewModels;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorBlogProject.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // Model
+            builder.Services.AddScoped<IModel, MemoryModel>();
+            // builder.Services.AddScoped<IModel, DbModel>();
+
+            // ViewModel
+            builder.Services.AddScoped<IVoegToeViewModel, VoegToeViewModel>();
+            builder.Services.AddScoped<ITotaalAantalViewModel, TotaalAantalViewModel>();
+
+            // Inlichtingendienst
+            builder.Services.AddScoped<Inlichtingendienst>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
